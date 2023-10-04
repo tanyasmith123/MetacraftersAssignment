3a4aaf4f6a19b9b6ac759ab6f7da7338504bfdfa
tanyasmith123 <146438590+tanyasmith123@users.noreply.github.com>
2023-10-04 13:30:25 +0000

    Update barbieCoin.sol

--- soliditybeginner/barbieCoin.sol
+++ soliditybeginner/barbieCoin.sol
@@ -1,1 +1,21 @@
-barbiecoin
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.18;
+
+contract BarbieCoin {
+    string public tokenName = "BarbieCoin";
+    string public tokenAbbrv = "BBC";
+    uint256 public totalSupply;
+
+    mapping(address => uint256) public balances;
+
+    function mint(address user, uint256 value) public  {
+        totalSupply += value;
+        balances[user] += value;
+    }
+
+    function burn(address user, uint256 value) public  {
+        require(balances[user] >= value, "Insufficient balance to burn");
+        totalSupply -= value;
+        balances[user] -= value;
+    }
+}
